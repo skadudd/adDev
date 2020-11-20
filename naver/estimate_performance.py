bbfbcf780ef4911738fbf648320fa24a1e528679
마케팅 <maketing@SILVYui-iMac.local>
2020-11-20 15:24:19 +0900

    get estimate performance by keyword

--- naver/estimate_performance.py
+++ naver/estimate_performance.py
@@ -28,7 +28,7 @@ def get_header(method, uri, api_key, secret_key, customer_id):
 
 
 def get_query():
-    df = pd.read_csv(monthly_performance_path + '/202011_CNC_구좌순위별_비딩가.csv',header=None)
+    df = pd.read_csv(monthly_performance_path + '/202011_CNC_test.csv',header=None)
     return df
 
 def get_rows(query):
@@ -49,28 +49,51 @@ def get_rows(query):
 
     return list_of_kwds,list_of_rows
 
+def handle_data(json_data):
+    kwd = json_data['keyword']
+    rank = pd.Series([1,2,3,4,5,6,7,8,9,10])
+    
+    df = pd.DataFrame.from_dict(json_data['estimate'])
+    df.insert(0,'rank',rank,allow_duplicates = False)
+    df.insert(0,'keyword',kwd,allow_duplicates = False)
+    df['cpc'] = round(df['cost']/df['clicks'],0)
+    df = df[['keyword','rank','bid','impressions','clicks','cpc','cost']]
+
+    return df
+
 def get_data(kwd_list, row_list):
     # bids[] : The maximum of 100 bids can be requested at once.
     uri = '/estimate/performance/keyword'
     method = 'POST'
-
+    handled_df = []
+    
     for v in range(len(kwd_list)):
         r = requests.post(BASE_URL + uri, json={'device': 'PC', 'keywordplus': True, 'key': kwd_list[v], 'bids':row_list[v] }, headers=get_header(method, uri, API_KEY, SECRET_KEY, CUSTOMER_ID))
 
         print("response status_code = {}".format(r.status_code))
         json_data = json.loads(r.text)
-        df_kwd = pd.DataFrame.from_dict(json_data['keyword'])
-        df_estimate = pd.DataFrame.from_dict(json_data['estimate'])
-        
-        #print("response body = {}".format(r.json()))
+        df = handle_data(json_data)
+        handled_df.append(df)
+
+    return handled_df
+
+def concat_df(data):
+    frames = []
+    for i in range(len(data)):
+        frames.append(data[i])
+    result = pd.concat(frames)
+
+    return result
 
 def init() :
     DB = []
     query = get_query()
     main_kwd = query[0][1]
     kwd_list,row_list = get_rows(query)
-    test = get_data(kwd_list,row_list)
+    list_of_df = get_data(kwd_list,row_list)
+    concated_df = concat_df(list_of_df)
 
+    print(concated_df)
     #print(kwd_list)
     #print(row_values)
 
