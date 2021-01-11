da79082f9c726860b41426cf07f55d56b32d1ae2
mkt <kim@mktui-iMac.local>
2021-01-11 19:17:09 +0900

    bo 스크래핑

---
+++ scrapper/bo_scrapper.py
@@ -0,0 +1,48 @@
+import time
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from pathlib import Path
+import pandas as pd
+
+def scroll_downer(driver):
+    last_height = 0
+    SCROLL_PAUSE_TIME = 3
+ 
+    while True:
+        # 화면 최하단으로 스크롤 다운
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
+        # 페이지 로드를 기다림
+        time.sleep(SCROLL_PAUSE_TIME)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight-50);")
+        time.sleep(SCROLL_PAUSE_TIME)
+    
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+    
+        # 새로운 높이가 이전 높이와 변하지 않았을 경우 스크롤 종료
+        if new_height == last_height:
+            break
+        
+        # 스크롤 다운이 된다면 스크롤 다운이 된 후의 창 높이를 새로운 높이로 갱신
+        last_height = new_height
+
+def get_infos(url,driver):
+    print('scrapping...',url)
+    driver.get(url)
+    html = driver.page_source
+    print(html)
+    soup = BeautifulSoup(html,'html.parser')
+    test = soup.select('div.MuiPaper-root MuiTableContainer-root MuiPaper-elevation1 MuiPaper-rounded')
+    print(test)
+
+def init():
+    url = 'https://bo.capa.ai/client'
+    driver = webdriver.Chrome('/Users/kim/Downloads/chromedriver')
+    driver.implicitly_wait(3)
+    scroll_downer(driver)
+    driver.get(url)
+    get_infos(url,driver)
+
+init()
