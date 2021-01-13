8e9b2d02671a8f9aa963049a729683a664a2f815
mkt <kim@mktui-iMac.local>
2021-01-13 16:12:25 +0900

    코드 정리

--- naver/get_nsa_report.py
+++ naver/get_nsa_report.py
@@ -5,7 +5,6 @@ import random
 import json
 import glob
 import requests
-# import openpyxl
 import signaturehelper
 import pandas as pd
 from pandas import DataFrame
@@ -15,21 +14,21 @@ from dateutil import rrule
 from datetime import datetime, timedelta
 from dateutil.parser import *
 
-#import key
-def get_header(method, uri, api_key, secret_key, customer_id):
-    timestamp = str(round(time.time() * 1000))
-    signature = signaturehelper.Signature.generate(timestamp, method, uri, SECRET_KEY)
-    return {'Content-Type': 'application/json; charset=UTF-8', 'X-Timestamp': timestamp, 'X-API-KEY': API_KEY, 'X-Customer': str(CUSTOMER_ID), 'X-Signature': signature}
-
-
 BASE_URL = 'https://api.naver.com'
 API_KEY = '0100000000adc996fd9c6660f2496ec0f64a1ce8c5688874b0d0f5074d8f98b0fadd4793b9'
 SECRET_KEY = 'AQAAAACtyZb9nGZg8kluwPZKHOjFpb6b54doFnHnBVokUWhNFw=='
 CUSTOMER_ID = '1158940'
-
 input_file = '../DB_for_GA_naver/'
 output_file = '../DB_of_naver_combined/'
-campaignList = ['cmp-a001-01-000000003407889', 'cmp-a001-01-000000003407888','cmp-a001-01-000000003645640','cmp-a001-01-000000003605317']
+campaignList = ['cmp-a001-01-000000003407889',
+                'cmp-a001-01-000000003407888',
+                'cmp-a001-01-000000003645640',
+                'cmp-a001-01-000000003605317',
+                'cmp-a001-01-000000003711127',
+                'cmp-a001-01-000000003711121',
+                'cmp-a001-01-000000003707633',
+                'cmp-a001-01-000000003707625'
+                ]
 
 #1. GET Summary Report per multiple entities 
 input_start_date = input('보고 시작 날짜 : ')
@@ -38,34 +37,35 @@ input_end_date = input('보고 종료 날짜 : ')
 start_date = parse(input_start_date)
 end_date = parse(input_end_date)
 
+#import key
+def get_header(method, uri, api_key, secret_key, customer_id):
+    timestamp = str(round(time.time() * 1000))
+    signature = signaturehelper.Signature.generate(timestamp, method, uri, SECRET_KEY)
+    return {'Content-Type': 'application/json; charset=UTF-8', 'X-Timestamp': timestamp, 'X-API-KEY': API_KEY, 'X-Customer': str(CUSTOMER_ID), 'X-Signature': signature}
+
+def iterate_request():
+    for dt in rrule.rrule(rrule.DAILY, dtstart=start_date, until=end_date):
+        date = str(dt.date())
+        try : 
+            data_to_csv(date)
+        except : 
+            print(f'no data in {date}')
 
 #requested data to csv
-def data_to_csv(date) :
-    
+def data_to_csv(date):
     for v in campaignList :
-        
-        #stat_ids = []
-        #stat_ids.append(v)
         uri = '/stats'
         method = 'GET'
-
         rangeOftime = {"since" : date, "until" : date}
         json_range_of_time = json.dumps(rangeOftime, indent=2)
-
-    #수정 20201110
         stat_ids = [v]
         params={
             'ids': stat_ids,
             'fields': '["clkCnt","impCnt","salesAmt"]', 
             'timeRange': json_range_of_time }
-    
-    
-        #request API
         r = requests.get(BASE_URL + uri, params, headers=get_header(method, uri, API_KEY, SECRET_KEY, CUSTOMER_ID))
-
         # print("response status_code = {}".format(r.status_code))
         # print("response body = {}".format(r.json()))
-
         json_data = json.loads(r.text)
 
         if(len(json_data['data']) == 0) :
@@ -73,6 +73,7 @@ def data_to_csv(date) :
             continue
         
         data = json_data['data'][0]
+
         print(v)
         print('campaign : ',data)
         striped_date = date.replace('-','')
@@ -82,12 +83,21 @@ def data_to_csv(date) :
         impCnt = data['impCnt']
         salesAmt = data['salesAmt']
         campaign = 0
+
         if(v == 'cmp-a001-01-000000003407889') :
             campaign = 'NSA_cnc_contents'
         elif(v == 'cmp-a001-01-000000003407888') :
             campaign = 'NSA_cnc'
+        elif(v == 'cmp-a001-01-000000003707625') :
+            campaign = 'NSA_cnc_Mobile'
         elif(v == 'cmp-a001-01-000000003645640') :
             campaign = 'NSA_3dp'
+        elif(v == 'cmp-a001-01-000000003707633') :
+            campaign = 'NSA_3dp_Mobile'
+        elif(v == 'cmp-a001-01-000000003711121') :
+            campaign = 'NSA_INJ'
+        elif(v == 'cmp-a001-01-000000003711127') : 
+            campaign = 'NSA_INJ_Mobile'
         elif(v == 'cmp-a001-01-000000003605317') :
             campaign = 'NSA_Brand'
 
@@ -97,11 +107,8 @@ def data_to_csv(date) :
         
         df.to_csv(Path(input_file, f'{striped_date} {source} {campaign}.csv'), index=False)
 
-def merge_csv() : 
-    #input_file = '/Users/maketing/adDev/DB_for_GA_naver/'
-    #output_file = '/Users/maketing/adDev/DB_of_naver_combined/'
+def merge_csv(): 
     allFile_list = glob.glob(os.path.join(input_file, '*.csv'))
-
     all_data = []
 
     for file in allFile_list:
@@ -111,15 +118,7 @@ def merge_csv() :
     data_combine = pd.concat(all_data, axis=0, ignore_index=True)
     data_combine.to_csv(Path(output_file, f'{start_date.date()}_{end_date.date()}_combined.csv'), index=False )
 
-def iterate_request() :
-    for dt in rrule.rrule(rrule.DAILY, dtstart=start_date, until=end_date):
-        date = str(dt.date())
-        try : 
-            data_to_csv(date)
-        except : 
-            print(f'no data in {date}')
-
-def removeAllFile() :
+def removeAllFile():
     if os.path.exists(input_file):
         for file in os.scandir(input_file):
             os.remove(file.path)
@@ -127,8 +126,9 @@ def removeAllFile() :
     else :
         return 'input 폴더 에러'
 
+def init():
+    iterate_request()
+    merge_csv()
+    removeAllFile()
 
-
-iterate_request()
-merge_csv()
-removeAllFile()
+init()
